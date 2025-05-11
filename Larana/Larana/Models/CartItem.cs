663329335019d1b9afa4ab981f2863621c0b42ec
Atif Kekec <kekecatf@outlook.com>
2025-05-11 16:02:45 +0300

    Revert "day4"
    This reverts commit 0f06b76901be222fc7c23142c96d26269b3c4955.

---
+++ Larana/Larana/Models/CartItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Larana.Models
+{
+    public class CartItem
+    {
+        public int Id { get; set; }
+        public int CartId { get; set; }
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+
+        [ForeignKey("CartId")]
+        public virtual Cart Cart { get; set; }
+
+        [ForeignKey("ProductId")]
+        public virtual Product Product { get; set; }
+    }
+}
